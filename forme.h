4685eea3a47bbab80b275bd7f5a3feb6abf2c4db
Ian-Mathieu Joly <joli1801@usherbrooke.ca>
2020-02-12 16:44:57 -0500

    Ajout forme.h

---
+++ forme.h
@@ -0,0 +1,63 @@
+#include <math.h>
+#define MAX_SIZE 5
+
+class Forme
+{
+    public:
+    void tourner();
+    void tourner(int direction);
+    void setX(int x1);
+    void setY(int y1);
+    int getX();
+    int getY();
+  
+    private:
+    int x;
+    int y;
+    int matrice[MAX_SIZE][MAX_SIZE];
+}
+int Forme::getX()
+{
+    return x;
+} 
+int Forme::getY()
+{
+    return y;
+} 
+void Forme::setX(int x1)
+{
+    x= x1;
+} 
+void Forme::setY(int y1)
+{
+    y= y1;
+} 
+
+void Forme:: tourner()
+{
+    int tmp[MAX_SIZE][MAX_SIZE];
+    
+    for(int i=0;i< MAX_SIZE;i++)
+    {
+        for(int j=0;j< MAX_SIZE;j++)
+        {
+             tmp[i][MAX_SIZE - j] = matrice[i][j];
+        }
+    }
+}
+}
+void Forme:: tourner(int direction)
+{
+    int tmp[MAX_SIZE][MAX_SIZE];
+    
+    for(int i=0;i< MAX_SIZE;i++)
+    {
+        for(int j=0;j< MAX_SIZE;j++)
+        {
+             tmp[MAX_SIZE - i][MAX_SIZE - j] = matrice[i][j];
+        }
+    }
+}
+
+
+
